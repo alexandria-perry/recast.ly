676000a2f43fa61497ad0b657eaae2636db5f0c5
alexandria-perry <aeperry22@gmail.com>
2018-11-26 19:56:36 -0800

    VideoListEntryDone

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,10 @@
-import exampleVideoData from '../data/exampleVideoData.js';
 import VideoListEntry from '../components/VideoListEntry.js';
 
 var VideoList = (props) => {
   return(
     <div className="video-list">
     {props.videos.map(video =>
-      <VideoListEntry video={exampleVideoData}/>
+      <VideoListEntry video={video}/>
     )}
   </div>
   )};
